4488e94cc39ea8172f8446e5e8e80851a17210d9
Samuel Walladge <samuel@opencraft.com>
2019-08-16 09:55:27 +0930

    debugging

--- cohort_manager/js-dist/cohort-manager.js
+++ cohort_manager/js-dist/cohort-manager.js
@@ -1,4 +1,6 @@
 RequireJS.define("cohort-manager", ["require", "exports", "react", "react-dom"], function (require, exports, React, ReactDOM) {
+    console.trace(React);
+    console.trace(ReactDOM);
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     class CohortManager extends React.Component {
